2c2f588e666309a08d9d2cacfc804c27ac3277a9
Ekaterina Kornishina <emkornishina@gmail.com>
2026-03-17 20:38:14 +0300

    add server example

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,21 @@
 import 'dotenv/config';
+import Fastify from 'fastify';
 
-const port = process.env.PORT;
+const port = Number(process.env.PORT || 4000);
 
 export const startServer = () => {
   console.log(`Server started at ${port}`);
+
+  const fastify = Fastify({ logger: true });
+
+  fastify.get('/', (request, reply) => {
+    reply.send({ hello: 'world' });
+  });
+
+  fastify.listen({ port }, (err, address) => {
+    if (err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
+  });
 };

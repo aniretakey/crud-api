264951658b19a90c76b019b1f2092c3819b19b73
Ekaterina Kornishina <emkornishina@gmail.com>
2026-03-21 18:37:38 +0300

    add products example

---
+++ src/db/products.ts
@@ -0,0 +1,9 @@
+import { Product } from '../types/types.ts';
+
+export const products: Product[] = [
+  { id: '1', name: 'Test', description: 'test test test', category: 'electronic', price: 100500, inStock: true },
+  { id: '2', name: 'Test2', description: 'test test test', category: 'books', price: 200, inStock: true },
+  { id: '3', name: 'Test3', description: 'test test test', category: 'clothing', price: 326, inStock: true },
+  { id: '4', name: 'Test4', description: 'test test test', category: 'books', price: 888, inStock: true },
+  { id: '5', name: 'Test5', description: 'test test test', category: 'cosmetic', price: 99.9, inStock: true },
+];

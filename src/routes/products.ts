b2d8579ef656c7ec0feb4098976be631bb1a44fb
Ekaterina Kornishina <emkornishina@gmail.com>
2026-03-21 20:44:25 +0300

    handle errors for product request

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify';
 import { findAllProducts, findProductById } from '../utils/utils.js';
 import { GetProductByIdRequest } from '../types/request.types.js';
+import { isValidUuid } from '../utils/helpers.js';
 
 async function productsRoutes(fastify: FastifyInstance) {
   fastify.get('/', async (request, reply) => {
@@ -10,9 +11,19 @@ async function productsRoutes(fastify: FastifyInstance) {
   });
 
   fastify.get('/:id', async (request: GetProductByIdRequest, reply) => {
-    const product = await findProductById(request.params.id);
+    const idFromReq = request.params.id;
 
-    // TODO: добавить обработку с 400 и 404 
+    // TODO: добавить обработку с 400 и 404
+
+    if (!isValidUuid(idFromReq)) {
+      return reply.code(400).send('Product id is not valid uuid');
+    }
+
+    const product = await findProductById(idFromReq);
+
+    if (!product) {
+      return reply.code(404).send(`Product with id ${idFromReq} not found`);
+    }
 
     return reply.code(200).send(product);
   });

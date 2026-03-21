7e8d1f119602182557da4a5ff551c13bd0a42973
Ekaterina Kornishina <emkornishina@gmail.com>
2026-03-21 18:38:02 +0300

    add functions for work with products (create, delete, etc.)

---
+++ src/utils/utils.ts
@@ -0,0 +1,52 @@
+import { randomUUID } from 'crypto';
+import { products } from '../db/products.js';
+import { Product } from '../types/types.js';
+
+export const findAllProducts = async () => {
+  return products;
+};
+
+export const findProductById = async (id: string) => {
+  return products.find(el => el.id === id);
+};
+
+export const addProduct = async (product: Omit<Product, 'id'>) => {
+  const id = randomUUID();
+
+  const newProduct = {
+    id: id,
+    ...product,
+  };
+
+  products.push(newProduct);
+
+  return newProduct;
+};
+
+export const deleteProduct = async (id: string) => {
+  const index = products.findIndex(el => el.id === id);
+
+  if (index === -1) {
+    return false;
+  }
+  products.splice(index, 1);
+
+  return true;
+};
+
+export const updateProduct = async (product: Product) => {
+  const index = products.findIndex(p => p.id === product.id);
+
+  if (index === -1) {
+    return null;
+  }
+
+  const updatedProduct: Product = {
+    ...products[index],
+    ...product,
+  };
+
+  products[index] = updatedProduct;
+
+  return updatedProduct;
+};

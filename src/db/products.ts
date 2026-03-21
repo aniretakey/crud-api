75205c67fd54edac07e5011f8820494d31b7e225
Ekaterina Kornishina <emkornishina@gmail.com>
2026-03-21 20:43:37 +0300

    update ids for product

--- src/db/products.ts
+++ src/db/products.ts
@@ -1,9 +1,44 @@
 import { Product } from '../types/types.ts';
 
 export const products: Product[] = [
-  { id: '1', name: 'Test', description: 'test test test', category: 'electronic', price: 100500, inStock: true },
-  { id: '2', name: 'Test2', description: 'test test test', category: 'books', price: 200, inStock: true },
-  { id: '3', name: 'Test3', description: 'test test test', category: 'clothing', price: 326, inStock: true },
-  { id: '4', name: 'Test4', description: 'test test test', category: 'books', price: 888, inStock: true },
-  { id: '5', name: 'Test5', description: 'test test test', category: 'cosmetic', price: 99.9, inStock: true },
+  {
+    id: '8bd924b4-cd52-47a1-bf3a-d484e4817231',
+    name: 'Test',
+    description: 'test test test',
+    category: 'electronic',
+    price: 100500,
+    inStock: true,
+  },
+  {
+    id: '4d84713b-9f73-451f-ba37-9af847f8d9a5',
+    name: 'Test2',
+    description: 'test test test',
+    category: 'books',
+    price: 200,
+    inStock: true,
+  },
+  {
+    id: '5b7bdc98-3265-42bf-b49c-8e27b9d9cf33',
+    name: 'Test3',
+    description: 'test test test',
+    category: 'clothing',
+    price: 326,
+    inStock: true,
+  },
+  {
+    id: 'd70c885f-8f86-42c3-a2f7-35f48fdaca1e',
+    name: 'Test4',
+    description: 'test test test',
+    category: 'books',
+    price: 888,
+    inStock: true,
+  },
+  {
+    id: 'a1d39e5a-2b94-4df8-9d08-c024e06f1b8b',
+    name: 'Test5',
+    description: 'test test test',
+    category: 'cosmetic',
+    price: 99.9,
+    inStock: true,
+  },
 ];
